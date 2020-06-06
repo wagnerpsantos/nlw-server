28e6e6b31c688c564260e76744d3a5ec7b939a4b
wpereira <wpereira@ciandt.com>
2020-06-06 18:39:15 -0300

    [NLW-AULA02] feat: include new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,15 @@
 import express from 'express';
-import Knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await Knex('items').select('*');
-
-    const serializedItems = items.map(item => {
-        return {
-            id: item.id,
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
-
-    return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-    const {
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-        items
-    } = request.body;
-
-    const trx = await Knex.transaction();
-
-    const ids = await trx('points').insert({
-        image: 'image-fake',
-        name,
-        email,
-        whatsapp,
-        latitude,
-        longitude,
-        city,
-        uf,
-    });
-
-    const point_id = ids[0];
-
-    const pointItems = items.map((item_id: number) => {
-        return {
-            item_id,
-            point_id,
-        }
-    });
-
-    await trx('point_items').insert(pointItems);
-
-    return response.json({ success: true });
-});
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 export default routes;
